8526543953cde63cbeb3f7f24b8008696bd06767
ebedeschi <emanuele.bedeschi@gmail.com>
2017-12-18 12:02:33 +0100

    lorawan stack

---
+++ Src/main.c
@@ -0,0 +1,356 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l0xx_hal.h"
+#include "adc.h"
+#include "i2c.h"
+#include "rtc.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* USER CODE BEGIN Includes */
+#include "hw.h"
+#include "low_power.h"
+#include "lora.h"
+#include "bsp.h"
+#include "timeServer.h"
+#include "vcom.h"
+#include "version.h"
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/*!
+ * Defines the application data transmission duty cycle. 5s, value in [ms].
+ */
+#define APP_TX_DUTYCYCLE                            10000
+/*!
+ * LoRaWAN Adaptive Data Rate
+ * @note Please note that when ADR is enabled the end-device should be static
+ */
+#define LORAWAN_ADR_ON                              0
+/*!
+ * LoRaWAN confirmed messages
+ */
+#define LORAWAN_CONFIRMED_MSG                    ENABLE
+/*!
+ * LoRaWAN application port
+ * @note do not use 224. It is reserved for certification
+ */
+#define LORAWAN_APP_PORT                            2
+/*!
+ * Number of trials for the join request.
+ */
+#define JOINREQ_NBTRIALS                            3
+
+/* Private macro -------------------------------------------------------------*/
+
+#define LOW_POWER_DISABLE
+/* Private function prototypes -----------------------------------------------*/
+
+/* call back when LoRa will transmit a frame*/
+static void LoraTxData( lora_AppData_t *AppData, FunctionalState* IsTxConfirmed);
+
+/* call back when LoRa has received a frame*/
+static void LoraRxData( lora_AppData_t *AppData);
+
+/* Private variables ---------------------------------------------------------*/
+/* load call backs*/
+static LoRaMainCallback_t LoRaMainCallbacks ={ HW_GetBatteryLevel,
+                                               HW_GetUniqueId,
+                                               HW_GetRandomSeed,
+                                               LoraTxData,
+                                               LoraRxData};
+
+/*!
+ * Specifies the state of the application LED
+ */
+static uint8_t AppLedStateOn = RESET;
+
+
+#ifdef USE_B_L072Z_LRWAN1
+/*!
+ * Timer to handle the application Tx Led to toggle
+ */
+static TimerEvent_t TxLedTimer;
+static void OnTimerLedEvent( void );
+#endif
+/* !
+ *Initialises the Lora Parameters
+ */
+static  LoRaParam_t LoRaParamInit= {TX_ON_TIMER,
+                                    APP_TX_DUTYCYCLE,
+                                    CLASS_A,
+                                    LORAWAN_ADR_ON,
+                                    DR_5,
+                                    LORAWAN_PUBLIC_NETWORK,
+                                    JOINREQ_NBTRIALS};
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+//  MX_GPIO_Init();
+//  MX_USART2_UART_Init();
+//  MX_I2C1_Init();
+//  MX_SPI2_Init();
+//  MX_SPI1_Init();
+//  MX_RTC_Init();
+//  MX_ADC_Init();
+
+  /* USER CODE BEGIN 2 */
+
+  /* Configure the hardware*/
+  HW_Init( );
+
+  /* Configure the Lora Stack*/
+  lora_Init( &LoRaMainCallbacks, &LoRaParamInit);
+
+  //PRINTF("VERSION: %X\n\r", VERSION);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	    /* run the LoRa class A state machine*/
+	    lora_fsm( );
+
+	    DISABLE_IRQ( );
+	    /* if an interrupt has occurred after DISABLE_IRQ, it is kept pending
+	     * and cortex will not enter low power anyway  */
+	    if ( lora_getDeviceState( ) == DEVICE_STATE_SLEEP )
+	    {
+	#ifndef LOW_POWER_DISABLE
+	      LowPower_Handler( );
+	#endif
+	    }
+	    ENABLE_IRQ();
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+//void SystemClock_Config(void)
+//{
+//
+//  RCC_OscInitTypeDef RCC_OscInitStruct;
+//  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+//  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+//
+//    /**Configure the main internal regulator output voltage
+//    */
+//  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+//
+//    /**Initializes the CPU, AHB and APB busses clocks
+//    */
+//  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_MSI;
+//  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+//  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+//  RCC_OscInitStruct.MSICalibrationValue = 0;
+//  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+//  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+//  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+//  {
+//    _Error_Handler(__FILE__, __LINE__);
+//  }
+//
+//    /**Initializes the CPU, AHB and APB busses clocks
+//    */
+//  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+//                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+//  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+//  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+//  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+//  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+//
+//  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+//  {
+//    _Error_Handler(__FILE__, __LINE__);
+//  }
+//
+//  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_I2C1
+//                              |RCC_PERIPHCLK_RTC;
+//  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+//  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+//  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+//  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+//  {
+//    _Error_Handler(__FILE__, __LINE__);
+//  }
+//
+//    /**Configure the Systick interrupt time
+//    */
+//  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+//
+//    /**Configure the Systick
+//    */
+//  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+//
+//  /* SysTick_IRQn interrupt configuration */
+//  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+//}
+
+/* USER CODE BEGIN 4 */
+
+static void LoraTxData( lora_AppData_t *AppData, FunctionalState* IsTxConfirmed)
+{
+  /* USER CODE BEGIN 3 */
+	uint32_t i = 0;
+
+	AppData->Buff[i++] = 'a';
+	AppData->Buff[i++] = 'b';
+	AppData->Buff[i++] = 'c';
+
+	AppData->Port = 1;
+
+	AppData->BuffSize = i;
+
+	*IsTxConfirmed =  LORAWAN_CONFIRMED_MSG;
+
+  /* USER CODE END 3 */
+}
+
+static void LoraRxData( lora_AppData_t *AppData )
+{
+  /* USER CODE BEGIN 4 */
+  switch (AppData->Port)
+  {
+  case LORAWAN_APP_PORT:
+
+    break;
+  default:
+    break;
+  }
+  /* USER CODE END 4 */
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void _Error_Handler(char * file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
